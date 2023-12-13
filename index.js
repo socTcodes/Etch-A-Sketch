bfd2384cf2634e9fd7950e8b473d0eeed537a4a5
SOCt33 <markshchukin13@gmail.com>
2023-12-13 14:32:23 -0800

    created button to return a new grid based on user input to fit grid

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-function createGrid(divs) {
-    const grid = document.getElementById('grid')
+function createGrid(size) {
+    const grid = document.getElementById('grid');
+    const squareSize = 100 / size; // Adjust this to set the size of each square
 
-    for (let i = 0; i < divs; i++) {
-        const div = document.createElement('div');
-        div.classList.add('grid-squares');
-        grid.appendChild(div);
-        div.addEventListener('mouseover', mouseOverGrid);
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement('div');
+            div.classList.add('grid-squares');
+            div.style.width = `${squareSize}%`;
+            div.style.height = `${squareSize}%`;
+            grid.appendChild(div);
+            div.addEventListener('mouseover', mouseOverGrid);
+        }
     }
 }
 
 function mouseOverGrid(event) {
-    const div = event.target;
     event.target.style.background = 'green';
 }
 
-createGrid(256);
+function newGridSize() {
+    const grid = document.getElementById('grid');
+    grid.innerHTML = '';
+
+    let userSize = prompt('Enter the size for the new grid (e.g., Enter 16 for a 16x16 grid (MAXIMUM 100)):');
+
+    if (userSize <= 0 || userSize > 100 || isNaN(userSize)) {
+        alert('Enter a valid number between 1 and 100!');
+        newGridSize();
+    } else {
+        createGrid(parseInt(userSize));
+    }
+}
+
+createGrid();
